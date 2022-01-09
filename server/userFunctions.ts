214b6fb7f39a5df29f831cc025def52bfd4ea266
Daniel Slattery <daniel.slattery1@gmail.com>
2022-01-09 22:56:45 +0000

    refactored server functions

--- server/userFunctions.ts
+++ server/userFunctions.ts
@@ -1,25 +1,28 @@
-module.exports = {
-  createUserAvatarUrl: function () {
+const createUserAvatarUrl = () => {
     const rand1 = Math.round(Math.random() * 151);
     return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${rand1}.png`;
-  },
-  shuffleAssignNextRound: function (num, users) {
+  }
+
+const shuffleAssignNextRound = (num, users) => {
     const values = Object.values(users);
     const onlyWithUserNames = values.filter(user => user.username !== undefined);
     let indexOfNextRound = Math.floor(Math.random() * (onlyWithUserNames.length - num));
     onlyWithUserNames.map((u, index) => {
       index === indexOfNextRound ? u.nextRound = true : u.nextRound = false;
     })
-  },
-  nextRoundSocketId: function (cb) {
+  }
+
+const nextRoundSocketId = (cb) => {
     const nextRoundUser = cb().find((user) => user.nextRound);
     if (nextRoundUser.socketId) return nextRoundUser.socketId;
-  },
-  allFinished: function (cb) {
+  }
+
+const allFinished = (cb) => {
     const onlyWithUserNames = cb();
     return onlyWithUserNames.every(el => el.isFinished === 'Finished ✔️')
-  },
-  nextRoundReset: function (cb) {
+  }
+
+const nextRoundReset = (cb) => {
     const onlyWithUserNames = cb();
     onlyWithUserNames.forEach((user) => user.isFinished = 'Drinking  🍺');
     const nextRoundUser = onlyWithUserNames.find((user) => user.nextRound);
@@ -33,7 +36,14 @@ module.exports = {
       }
     }
   }
-}
+
+  module.exports = {
+    createUserAvatarUrl,
+    shuffleAssignNextRound,
+    nextRoundSocketId,
+    nextRoundReset,
+    allFinished,
+  }
 
 
 
