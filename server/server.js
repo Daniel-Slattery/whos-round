44247946291cd551bca2f7bbf9bcc0872b484625
Daniel Slattery <daniel.slattery1@gmail.com>
2022-01-10 00:30:55 +0000

    refactor server

--- server/server.js
+++ server/server.js
@@ -5,89 +5,95 @@ const {
   nextRoundReset,
   nextRoundSocketId,
   shuffleAssignNextRound,
-  users,
-} = require("./userFunctions.ts");
-const io = require("socket.io")();
-const uuidv1 = require("uuid/v1");
+  users
+} = require('./userFunctions.ts')
+const io = require('socket.io')()
 
-console.log("Server Started! 🚀");
+console.log('Server Started! 🚀')
 
-
-io.on("connection", (socket) => {
-  users[socket.id] = { userId: uuidv1() };
-  const userSocket = users[socket.id];
-  socket.on("disconnect", () => {
-    userSocket.nextRound && shuffleAssignNextRound(1, users);
-    delete users[socket.id];
-    io.emit("action", {
-      type: "users_online",
-      data: createUsersOnline(),
-    });
-  });
-  socket.on("action", (action) => {
+io.on('connection', socket => {
+  users[socket.id] = {userId: socket.id}
+  console.log('users', users)
+  const userSocket = users[socket.id]
+  socket.on('disconnect', () => {
+    userSocket.nextRound && shuffleAssignNextRound(1)
+    delete users[socket.id]
+    io.emit('action', {
+      type: 'users_online',
+      data: createUsersOnline()
+    })
+  })
+  socket.on('action', action => {
     switch (action.type) {
-      case "server/hello":
-        socket.emit("action", {
-          type: "message",
-          data: "Good day from the server!",
-        });
-        break;
-      case "server/join":
-        console.log(`user ${action.inputName} connected`);
-        userSocket.username = action.inputName;
-        userSocket.drink = action.inputDrink;
-        userSocket.avatar = createUserAvatarUrl();
-        userSocket.isFinished = "Drinking  🍺";
-        userSocket.nextRound = false;
-        userSocket.socketId = socket.id;
-        shuffleAssignNextRound(0, users);
-        io.emit("action", {
-          type: "users_online",
-          data: createUsersOnline(),
-        }); //io emit includes sender, socket emit only sends to others
-        break;
-      case "server/finished": //handling when beer icon is pressed
-        userSocket.isFinished === "Drinking  🍺"
-          ? (userSocket.isFinished = "Finished ✔️")
-          : (userSocket.isFinished = "Drinking  🍺");
+      case 'server/hello':
+        socket.emit('action', {
+          type: 'message',
+          data: 'Good day from the server!'
+        })
+        break
+      case 'server/join':
+        console.log(`user ${action.inputName} connected`)
+        // userSocket = {
+        //   ...userSocket,
+        //   username: action.inputName,
+        //   drink: action.inputDrink,
+        //   avatar: createUserAvatarUrl(),
+        //   isFinished: 'Drinking  🍺',
+        //   nextRound: false,
+        //   socketId: socket.id
+        // }
+        userSocket.username = action.inputName
+        userSocket.drink = action.inputDrink
+        userSocket.avatar = createUserAvatarUrl()
+        userSocket.isFinished = 'Drinking  🍺'
+        userSocket.nextRound = false
+        userSocket.socketId = socket.id
+        shuffleAssignNextRound(0)
+        io.emit('action', {
+          type: 'users_online',
+          data: createUsersOnline()
+        }) //io emit includes sender, socket emit only sends to others
+        break
+      case 'server/finished': //handling when beer icon is pressed
+        userSocket.isFinished === 'Drinking  🍺'
+          ? (userSocket.isFinished = 'Finished ✔️')
+          : (userSocket.isFinished = 'Drinking  🍺')
         //check if all users are finished drinks
-        if (allFinished(createUsersOnline)) {
-          io.to(nextRoundSocketId(createUsersOnline)).emit("action", {
-            type: "private_message",
-            data: true,
-          });
-          io.emit("action", { type: "next_round", data: true });
+        if (allFinished()) {
+          io.to(nextRoundSocketId()).emit('action', {
+            type: 'private_message',
+            data: true
+          })
+          io.emit('action', {type: 'next_round', data: true})
         } else {
-          io.to(nextRoundSocketId(createUsersOnline)).emit("action", {
-            type: "private_message",
-            data: false,
-          });
-          io.emit("action", { type: "next_round", data: false });
+          io.to(nextRoundSocketId()).emit('action', {
+            type: 'private_message',
+            data: false
+          })
+          io.emit('action', {type: 'next_round', data: false})
         }
-        userSocket.isFinished === "Finished ✔️"
-          ? io
-              .to(socket.id)
-              .emit("action", { type: "user_finished", data: true })
+        userSocket.isFinished === 'Finished ✔️'
+          ? io.to(socket.id).emit('action', {type: 'user_finished', data: true})
           : io
               .to(socket.id)
-              .emit("action", { type: "user_finished", data: false });
-        io.emit("action", {
-          type: "users_online",
-          data: createUsersOnline(),
-        });
-        break;
-      case "server/nextRound":
-        nextRoundReset(createUsersOnline);
-        io.emit("action", { type: "next_round", data: false });
-        io.emit("action", { type: "private_message", data: false });
-        io.emit("action", { type: "user_finished", data: false });
-        io.emit("action", {
-          type: "users_online",
-          data: createUsersOnline(),
-        });
-        break;
+              .emit('action', {type: 'user_finished', data: false})
+        io.emit('action', {
+          type: 'users_online',
+          data: createUsersOnline()
+        })
+        break
+      case 'server/nextRound':
+        nextRoundReset()
+        io.emit('action', {type: 'next_round', data: false})
+        io.emit('action', {type: 'private_message', data: false})
+        io.emit('action', {type: 'user_finished', data: false})
+        io.emit('action', {
+          type: 'users_online',
+          data: createUsersOnline()
+        })
+        break
     }
-  });
-});
+  })
+})
 
-io.listen(3001);
+io.listen(3001)
--- server/userFunctions.ts
+++ server/userFunctions.ts
@@ -1,62 +1,70 @@
-const users = {};
+const users = {}
 
 const createUsersOnline = () => {
-  const values = Object.values(users);
-  const onlyWithUserNames = values.filter(
-    (user) => user.username !== undefined
-  );
-  return onlyWithUserNames;
+  const values = Object.values(users)
+  const onlyWithUserNames = values.filter(user => user.username !== undefined)
+  return onlyWithUserNames
 }
 
 const createUserAvatarUrl = () => {
-    const rand1 = Math.round(Math.random() * 151);
-    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${rand1}.png`;
-  }
+  const rand1 = Math.round(Math.random() * 151)
+  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${rand1}.png`
+}
 
-const shuffleAssignNextRound = (num, users) => {
-    const values = Object.values(users);
-    const onlyWithUserNames = values.filter(user => user.username !== undefined);
-    let indexOfNextRound = Math.floor(Math.random() * (onlyWithUserNames.length - num));
-    onlyWithUserNames.map((u, index) => {
-      index === indexOfNextRound ? u.nextRound = true : u.nextRound = false;
-    })
-  }
+const shuffleAssignNextRound = num => {
+  const values = Object.values(users)
+  const onlyWithUserNames = values.filter(user => user.username !== undefined)
+  let indexOfNextRound = Math.floor(
+    Math.random() * (onlyWithUserNames.length - num)
+  )
+  onlyWithUserNames.map((u, index) => {
+    index === indexOfNextRound ? (u.nextRound = true) : (u.nextRound = false)
+  })
+}
 
 const nextRoundSocketId = () => {
-    const nextRoundUser = createUsersOnline().find((user) => user.nextRound);
-    if (nextRoundUser.socketId) return nextRoundUser.socketId;
-  }
+  const nextRoundUser = createUsersOnline().find(user => user.nextRound)
+  if (nextRoundUser.socketId) return nextRoundUser.socketId
+}
 
 const allFinished = () => {
-    const onlyWithUserNames = createUsersOnline();
-    return onlyWithUserNames.every(el => el.isFinished === 'Finished ✔️')
-  }
+  const onlyWithUserNames = createUsersOnline()
+  return onlyWithUserNames.every(el => el.isFinished === 'Finished ✔️')
+}
 
 const nextRoundReset = () => {
-    const onlyWithUserNames = createUsersOnline();
-    onlyWithUserNames.forEach((user) => user.isFinished = 'Drinking  🍺');
-    const nextRoundUser = onlyWithUserNames.find((user) => user.nextRound);
-    currentNextRoundIndex = onlyWithUserNames.indexOf(nextRoundUser);
-    if (onlyWithUserNames[currentNextRoundIndex].nextRound) {
-      onlyWithUserNames[currentNextRoundIndex].nextRound = false;
-      if (currentNextRoundIndex === (onlyWithUserNames.length - 1)) {
-        onlyWithUserNames[0].nextRound = true;
-      } else {
-        onlyWithUserNames[currentNextRoundIndex + 1].nextRound = true;
-      }
+  const onlyWithUserNames = createUsersOnline()
+  onlyWithUserNames.forEach(user => (user.isFinished = 'Drinking  🍺'))
+  const nextRoundUser = onlyWithUserNames.find(user => user.nextRound)
+  currentNextRoundIndex = onlyWithUserNames.indexOf(nextRoundUser)
+  if (onlyWithUserNames[currentNextRoundIndex].nextRound) {
+    onlyWithUserNames[currentNextRoundIndex].nextRound = false
+    if (currentNextRoundIndex === onlyWithUserNames.length - 1) {
+      onlyWithUserNames[0].nextRound = true
+    } else {
+      onlyWithUserNames[currentNextRoundIndex + 1].nextRound = true
     }
   }
+}
 
-  module.exports = {
-    allFinished,
-    createUserAvatarUrl,
-    createUsersOnline,
-    nextRoundReset,
-    nextRoundSocketId,
-    shuffleAssignNextRound,
-    users,
+const createNewUser = () => {
+  const newUser = {
+    avatar: createUserAvatarUrl(),
+    drink: '',
+    isFinished: 'Drinking  🍺',
+    nextRound: false,
+    socketId: '',
+    username: ''
   }
+  return newUser
+}
 
-
-
-
+module.exports = {
+  allFinished,
+  createUserAvatarUrl,
+  createUsersOnline,
+  nextRoundReset,
+  nextRoundSocketId,
+  shuffleAssignNextRound,
+  users
+}
